791ca86c955470579f21479519937b8aaaf2ff97
ThanhNhon <ngothanhnhonnh@gmail.com>
2024-03-01 17:21:54 +0700

    second add

---
+++ frontend/src/layout/MainWrapper.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { setUser } from "../utils/auth";
+
+const MainWrapper = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  useEffect(async () => {
+    const handler = async () => {
+      setLoading(true);
+      await setUser();
+      setLoading(false);
+    };
+    handler();
+  }, []);
+  return <>{loading ? null : children}</>;
+};
+
+export default MainWrapper;
