abe7710191d90b4174cfd02227101c1afb095760
ThanhNhon <ngothanhnhonnh@gmail.com>
2024-03-06 17:24:21 +0700

    add db vendor, category, product, cart, review

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from store.models import Product, Category, Gallery, Specification, Size, Color, Cart, CartOrder, CartOrderItem, Review
 # Register your models here.
+
+class GalleryInline(admin.TabularInline):
+    model = Gallery
+
+class SpecificationInline(admin.TabularInline):
+    model = Specification
+
+class SizeInline(admin.TabularInline):
+    model = Size
+
+class ColorInline(admin.TabularInline):
+    model = Color
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'category', 'shipping_amount', 'stock_qty', 'in_stock', 'vendor', 'featured']
+    list_editable = ['featured']
+    list_filter = ['date']
+    search_fields = ['title']
+    inlines = [GalleryInline, SpecificationInline, SizeInline, ColorInline]
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(CartOrder)
+admin.site.register(CartOrderItem)
+admin.site.register(Review, ReviewAdmin)
--- backend/store/models.py
+++ backend/store/models.py
@@ -1,3 +1,278 @@
 from django.db import models
+from vendor.models import Vendor
+from userauths.models import User, Profile
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+ 
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="category", default="category.jpg", null=True, blank=True)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(unique = True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Category"
+        ordering = ['-title']
+
+class Product(models.Model):
+
+    STATUS = (
+        ("draft", "Draft"),
+        ("disabled", "Disabled"),
+        ("in_review", "In Review"),
+        ("published", "Published"),
+    )
+
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="products", default="product.jpg", null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null= True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=12, default = 0.00)
+    old_price = models.DecimalField(decimal_places=2, max_digits=12, default = 0.00)
+    shipping_amount = models.DecimalField(decimal_places=2, max_digits=12, default = 0.00)
+    stock_qty = models.PositiveIntegerField(default=1)
+    in_stock = models.BooleanField(default=True)
+    status = models.CharField(max_length=100, choices = STATUS, default = "published")
+    featured = models.BooleanField(default=False)
+    view = models.PositiveIntegerField(default=0)
+    rating = models.PositiveIntegerField(default=0)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    pid = ShortUUIDField(unique=True, length = 10, alphabet = "abcdefg12345")
+    slug = models.SlugField(unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.name)
+
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+    def product_rating(self):
+        product_rating = Review.objects.filter(product = self).aggregate(avg_rating=models.Avg("rating"))
+        return product_rating['avg_rating']
+    
+    def save(self, *args, **kwargs):
+        self.rating = self.product_rating()
+        super(Product,self).save(*args, **kwargs)
+    
+
+class Gallery(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    image = models.FileField(upload_to="products", default="product.jpg", null=True, blank=True)
+    active = models.BooleanField(default=True)
+    gid = ShortUUIDField(max_length = 25, length = 10, alphabet = "abcdefg12345")
+
+    def __str__(self):
+        return self.product.title
+    
+    class Meta:
+        verbose_name_plural = "Product Images"
+    
+class Specification(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Size(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=12, default = 0.00)
+
+    def __str__(self):
+        return self.name
+    
+class Color(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    color_code = models.CharField(max_length=100)
+
+
+
+    def __str__(self):
+        return self.name
+    
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    qty = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    sub_total = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    shipping_amount = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    service_fee = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    tax_fee = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    total = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    size = models.CharField(max_length=100, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
+    cart_id = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.cart_id}-{self.product.title}"
+
+class CartOrder(models.Model):
+
+
+    PAYMENT_STATUS = (
+        ("paid", "Paid"),
+        ("pending", "Pending"),
+        ("processing", "Processing"),
+        ("cancelled", "Cancelled"),
+    )
+
+    ORDER_STATUS = (
+        ("pending", "Pending"),
+        ("fulfilled", "Fulfilled"),
+        ("cancelled", "Cancelled"),
+    )
+
+    vendor = models.ManyToManyField(Vendor, blank=True)
+    buyer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    sub_total = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    shipping_amount = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    service_fee = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    tax_fee = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    total = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+
+    payment_status = models.CharField(choices=PAYMENT_STATUS, max_length=100, default = "pending")
+    order_status = models.CharField(choices = ORDER_STATUS, max_length=100, default = "pending")
+
+    #Coupons
+    initial_total = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    saved = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    
+    #Bio Data
+    full_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    mobile = models.CharField(max_length=100, blank=True, null=True)
+
+    #shipping address
+    address = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+
+    oid = ShortUUIDField(unique=True, length = 10, alphabet = "abcdefg12345")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.oid
+    
+class CartOrderItem(models.Model):
+    order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    qty = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    sub_total = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    shipping_amount = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    service_fee = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    tax_fee = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    total = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    size = models.CharField(max_length=100, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
+
+    initial_total = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+    saved = models.DecimalField(default=0.00, decimal_places=2, max_digits=12)
+
+    oid = ShortUUIDField(unique=True, length = 10, alphabet = "abcdefg12345")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.oid
+    
+class ProductFaq(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    email = models.EmailField(null=True, blank=True)
+    question = models.CharField(max_length=100)
+    answer = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+    
+    class Meta:
+        verbose_name_plural = "Product FAQs"
+
+class Review(models.Model):
+
+    RATING = (
+        (1,"1 star"),
+        (2,"2 star"),
+        (3,"3 star"),
+        (4,"4 star"),
+        (5,"5 star"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    review = models.TextField()
+    reply = models.TextField(null=True, blank=True)
+    rating = models.IntegerField(default=None, choices=RATING)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.product.title
+    
+    class Meta:
+        verbose_name_plural = "Review&Rating"
+
+    def profile(self):
+        return Profile.objects.get(user=self.user)
+
+@receiver(post_save, sender=Review)
+def update_product_rating(sender, instance, **kwargs):
+    if instance.product:
+        instance.product.save()
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.title
+    
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    order = models.ForeignKey(CartOrder, on_delete=models.SET_NULL, null=True, blank=True)
+    order_item = models.ForeignKey(CartOrderItem, on_delete=models.SET_NULL, null=True, blank=True)
+    seen = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.order:
+            return self.order.oid
+        else:
+            return f"Notification - {self.pk}"
+    
+class Coupon(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    user_by = models.ManyToManyField(User, blank=True)
+    code = models.CharField(max_length=1000)
+    discount = models.IntegerField(default=1)
+    active = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.code
+    
